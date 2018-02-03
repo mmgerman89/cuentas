cb70790eb6caa27fe7d3319836d2115992dffe62
Germán Martínez <mmgerman89@gmail.com>
2018-02-03 02:58:59 -0300

    actualizacion de casa

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,6 @@
 class HomesController < ApplicationController
+  before_action :set_home, only: [:edit, :show, :update]
+
   def new
     @home = Home.new()
   end
@@ -15,16 +17,33 @@ class HomesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @home.update(home_params)
+      flash[:success] = "Casa actualiza con éxito"
+      redirect_to homes_path
+    else
+      flash[:danger] = "Error al actualizar casa"
+      render 'new'
+    end
+  end
+
   def index
     @homes = current_user.homes
   end
 
   def show
-    @home = Home.find(params[:id])
+    
   end
 
   private
     def home_params
       params.require(:home).permit(:name, :address)
+    end
+
+    def set_home
+      @home = Home.find(params[:id])
     end
 end
